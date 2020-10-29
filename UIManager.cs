b9d948c1bbc051a8b0d41ad2bd929d515f9c3b32
Gatene1 <65918616+Gatene1@users.noreply.github.com>
2020-10-28 23:29:56 -0400

    Create UIManager.cs

---
+++ UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _coinText, _livesText;
+
+    public void UpdateCoinDisplay(int coins)
+    {
+        _coinText.text = "Coins: " + coins.ToString();
+    }
+
+    public void UpdateLivesDisplay(int lives)
+    {
+        _livesText.text = "Lives: " + lives.ToString();
+    }
+   
+}
